5d5cf9721953191993b3ece32f4845e598aff45c
Bach Nguyen <ngb1998@gmail.com>
2020-10-06 09:30:16 -0700

    update

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 # pages/urls.py
 from django.urls import path
-from .views import homePageView
+from .views import HomePageView, AboutPageView
 
 urlpatterns = [
-    path('', homePageView, name='home')
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
-def homePageView(request):
-    return HttpResponse('hello world')
+# def homePageView(request):
+#     return HttpResponse('hello world')
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+# def upload(request):
+#     return
